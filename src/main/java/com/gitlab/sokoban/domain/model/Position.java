ecbfa264930d617fd9d408e68a6e1ecbb828ed9b
Natsukiraa <gazio.mael@gmail.com>
2025-02-18 10:22:34 +0100

    Position

--- src/main/java/com/gitlab/sokoban/domain/model/Position.java
+++ src/main/java/com/gitlab/sokoban/domain/model/Position.java
@@ -1,4 +1,15 @@
 package com.gitlab.sokoban.domain.model;
 
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int[] getPosition() {
+        return new int[] {x, y};
+    }
 }
