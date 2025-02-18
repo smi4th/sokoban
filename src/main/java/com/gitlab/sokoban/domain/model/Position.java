824c849b8fc0048907ecdbd687d6fa102a0a5b4a
Natsukiraa <gazio.mael@gmail.com>
2025-02-18 10:26:15 +0100

    added tests

--- src/main/java/com/gitlab/sokoban/domain/model/Position.java
+++ src/main/java/com/gitlab/sokoban/domain/model/Position.java
@@ -12,4 +12,12 @@ public class Position {
     public int[] getPosition() {
         return new int[] {x, y};
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
 }
---
+++ src/test/java/com/gitlab/sokoban/domain/model/TestPosition.java
@@ -0,0 +1,21 @@
+package com.gitlab.sokoban.domain.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPosition {
+
+    @Test
+    public void testPositionsAreEqual() {
+        Position pos1 = new Position(1, 2);
+        Position pos2 = new Position(1, 2);
+        assertEquals(pos1, pos2, "Positions with the same coordinates should be equal");
+    }
+
+    @Test
+    public void testPositionsAreNotEqual() {
+        Position pos1 = new Position(1, 2);
+        Position pos2 = new Position(2, 3);
+        assertNotEquals(pos1, pos2, "Positions with different coordinates should not be equal");
+    }
+}
