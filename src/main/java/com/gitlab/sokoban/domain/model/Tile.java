5f04a182166139347227bb0b9bc52cfcae06f63e
Akas <phelina94@gmail.com>
2025-02-18 10:25:08 +0100

    tt

--- src/main/java/com/gitlab/sokoban/domain/model/Tile.java
+++ src/main/java/com/gitlab/sokoban/domain/model/Tile.java
@@ -1,4 +1,19 @@
 package com.gitlab.sokoban.domain.model;
 
 public class Tile {
+    private Position position;
+    private State state;
+
+    public Tile(Position position, State state) {
+        this.position = position;
+        this.state = state;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public State getState(){
+        return state;
+    }
 }
