e48c89723e138459a12f81e5527d09eb800adc53
smiath <smiath.yt@gmail.com>
2025-02-18 10:51:01 +0100

    [ADD]: MapBuilder

--- src/main/java/com/gitlab/sokoban/domain/model/MapBuilder.java
+++ src/main/java/com/gitlab/sokoban/domain/model/MapBuilder.java
@@ -1,4 +1,35 @@
 package com.gitlab.sokoban.domain.model;
 
 public class MapBuilder {
+
+    public MapBuilder() {}
+
+    public static Map build(String map) {
+        Tile[][] tiles = toTiles(map);
+        return new Map(tiles[0].length, tiles.length, tiles);
+    }
+
+    public static Tile[][] toTiles(String map) {
+        String[] lines = map.split("\n");
+        Tile[][] tiles = new Tile[lines.length][lines[0].length()];
+        for (int y = 0; y < lines.length; y++) {
+            for (int x = 0; x < lines[y].length(); x++) {
+                tiles[y][x] = new Tile(new Position(x, y), toState(lines[y].charAt(x)));
+            }
+        }
+        return tiles;
+    }
+
+    private static State toState(char c) {
+        return switch (c) {
+            case '@' -> State.PLAYER;
+            case '#' -> State.WALL;
+            case ' ' -> State.EMPTY;
+            case '$' -> State.BOX;
+            case '.' -> State.STORAGE;
+            case '*' -> State.BOX_STORED;
+            default -> throw new IllegalArgumentException("Invalid character: " + c);
+        };
+    }
+
 }
