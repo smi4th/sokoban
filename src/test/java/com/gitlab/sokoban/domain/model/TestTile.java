4552cb8fccbf220b824bf20b51741ee70f7b3929
Akas <phelina94@gmail.com>
2025-02-18 10:43:57 +0100

    gkdfhlgj

--- src/test/java/com/gitlab/sokoban/domain/model/TestTile.java
+++ src/test/java/com/gitlab/sokoban/domain/model/TestTile.java
@@ -29,4 +29,27 @@ public class TestTile {
         assertNotEquals(tile1, tile2, "Tiles with different position and state should not be equal");
     }
 
+    // Position pas égal, état égal == faux
+    @Test
+    public void testTwoTilesPositionEqualStateNotEqual(){
+        Position pos1 = new Position(1,2);
+        Position pos2 = new Position(1, 2);
+
+        Tile tile1 = new Tile(pos1, State.PLAYER);
+        Tile tile2 = new Tile(pos2, State.WALL);
+
+        assertNotEquals(tile1, tile2, "Tiles with same position and different state should not be equal");
+    }
+
+    // Position égal, état pas égal == faux
+    @Test
+    public void testTwoTilesPositionNotEqualStateEqual(){
+        Position pos1 = new Position(1,2);
+        Position pos2 = new Position(1, 3);
+
+        Tile tile1 = new Tile(pos1, State.WALL);
+        Tile tile2 = new Tile(pos2, State.WALL);
+
+        assertNotEquals(tile1, tile2, "Tiles with different position and same state should not be equal");
+    }
 }
